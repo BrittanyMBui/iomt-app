64d745cfb1311beb583c303e005457a7e23e8992
brittanymbui <brittymbui@gmail.com>
2021-03-05 09:58:49 -0800

    Formats date string

--- src/components/posts/CreatePost.js
+++ src/components/posts/CreatePost.js
@@ -3,7 +3,7 @@ import { Editor } from '@tinymce/tinymce-react';
 
 class CreatePost extends React.Component {
     state = {
-        title: new Date(),
+        title: new Date().toDateString(),
         body: '',
     }
 
@@ -68,12 +68,12 @@ class CreatePost extends React.Component {
                             plugins: [
                                 'advlist autolink lists link image charmap print preview anchor',
                                 'searchreplace visualblocks code fullscreen',
-                                'insertdatetime media table paste code help wordcount'
+                                'insertdatetime media table paste code help wordcount',
                             ],
                             toolbar:
                                 'undo redo | formatselect | bold italic backcolor | \
                                 alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | removeformat | help'
+                                bullist numlist | removeformat | help'
                         }}
                     />
                     <br />
--- src/components/posts/EditPost.js
+++ src/components/posts/EditPost.js
@@ -73,12 +73,12 @@ class EditPost extends React.Component {
                             plugins: [
                                 'advlist autolink lists link image charmap print preview anchor',
                                 'searchreplace visualblocks code fullscreen',
-                                'insertdatetime media table paste code help wordcount'
+                                'insertdatetime media table paste code help wordcount',
                             ],
                             toolbar:
                                 'undo redo | formatselect | bold italic backcolor | \
                                 alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | removeformat | help'
+                                bullist numlist | removeformat | help'
                         }}
                     />
                     <br />
