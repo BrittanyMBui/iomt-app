8c47117f7e356dc98542687885db14cd6760f43f
brittanymbui <brittymbui@gmail.com>
2021-03-06 14:28:24 -0800

    Refactors signup route

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,78 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-class SignUp extends React.Component {
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        posts: [],
-        articles: []
-    }
+const SignUp = () => {
+    const history = useHistory();
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [cnfPassword, setCnfPassword] = useState();
+    const [error, setError] = useState(null);
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.id]: event.target.value
-        })
-    };
-
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state);
-        fetch('http://localhost:4000/iomtapi/v1/users/signup', {
+        if (!username || !email || !password || !cnfPassword) {
+            return setError('All fields are required');
+        }
+        if (password !== cnfPassword) {
+            return setError('Passwords do not match');
+        }
+        const newUser = { username, email, password };
+        fetch('https://safe-fortress-45916.herokuapp.com/iomtapi/v1/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(this.state)
+            body: JSON.stringify(newUser)
         })
         .then((response) => {
-            return response.json();
+            if (response.status === 200) {
+                return response.json();
+            }
+            setError(response.message)
+            
         })
         .then((jsonData) => {
             console.log(jsonData);
-            this.props.history.push('/home')
+            history.push('/users/login')
         })
         .catch((err) => console.log(err));
     };
 
 
-    render() {
-        return (
-            <div>
-                <h1>Create an IoMT Account</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="username">Username</label>
-                    <input 
-                        onChange={this.handleChange}
-                        value={this.state.username}
-                        type="text"
-                        id="username"
-                        name="username"
-                    />
-                    <br />
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        onChange={this.handleChange}
-                        value={this.state.email}
-                        type="email"
-                        id="email"
-                        name="email"
-                    />
-                    <br />
-                    <label htmlFor="password">Password</label>
-                    <input 
-                        onChange={this.handleChange}
-                        value={this.state.password}
-                        type="password"
-                        id="password"
-                        name="password"
-                    />
-                    <br />
-                    <button type="submit">Create Account</button>
-                </form>
-            </div>
-        );
-    }
+    
+    return (
+        <div>
+            <h1>Create an IoMT Account</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    onChange={(event) => setUsername(event.target.value)}
+                    value={username}
+                    type="text"
+                    id="username"
+                    name="username"
+                />
+                <br />
+                <label htmlFor="email">Email</label>
+                <input 
+                    onChange={(event) => setEmail(event.target.value)}
+                    value={email}
+                    type="email"
+                    id="email"
+                    name="email"
+                />
+                <br />
+                <label htmlFor="password">Password</label>
+                <input 
+                    onChange={(event) => setPassword(event.target.value)}
+                    value={password}
+                    type="password"
+                    id="password"
+                    name="password"
+                />
+                <br />
+                <label htmlFor="cnfPassword">Confirm Password</label>
+                <input 
+                    onChange={(event) => setCnfPassword(event.target.value)}
+                    value={cnfPassword}
+                    type="password"
+                    id="cnfPassword"
+                    name="cnfPassword"
+                />
+                <br />
+                <button type="submit">Create Account</button>
+            </form>
+        </div>
+    );
 };
 
 export default SignUp;
